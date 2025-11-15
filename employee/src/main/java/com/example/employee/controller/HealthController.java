4b02cfd6a9149a8503ee4dc00f6fd03ca8449c90
Prince Jaiswal <princejaiswal0920@gmail.com>
2025-11-15 14:53:44 +0530

    added health controller for uptime robot

---
+++ employee/src/main/java/com/example/employee/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.example.employee.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
